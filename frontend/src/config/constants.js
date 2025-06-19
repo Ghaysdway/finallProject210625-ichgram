6cc613d0afded475d62eba71bcb12bf1971bbd41
ghays <furore-plus@ukr.net>
2025-06-19 18:32:27 +0200

    defaultPost

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,9 +1,10 @@
 import defaultLogo from "../assets/logo.png";
 import defaultAvatar from "../assets/bird.png";
+import defaultPostImage from "../assets/defaultPost.jpg";
 
 export const API_URL = "http://localhost:3000";
 export const UPLOADS_URL = `${API_URL}/uploads`;
 export const LOGO_URL = defaultLogo;
 
 export const DEFAULT_AVATAR = defaultAvatar;
-export const DEFAULT_POST_IMAGE = "https://picsum.photos/600";
+export const DEFAULT_POST_IMAGE = defaultPostImage;
